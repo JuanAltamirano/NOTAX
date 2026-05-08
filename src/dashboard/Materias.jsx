9c4d7875e82d64c0c0169e5a6d0b294a6294e4b6
DavidQuira <alejandroquira20@gmail.com>
2026-05-08 14:48:53 -0500

    materias

--- src/dashboard/Materias.jsx
+++ src/dashboard/Materias.jsx
@@ -1,9 +1,77 @@
+import { useState } from "react";
+
 import "./Ventanas.css";
 
 function Materias() {
+
+  const [materia, setMateria] = useState("");
+
+  const [materias, setMaterias] = useState([
+    {
+      nombre: "Programación",
+      promedio: 3.8,
+    },
+
+    {
+      nombre: "Matemáticas",
+      promedio: 3.2,
+    },
+  ]);
+
+  const agregarMateria = () => {
+
+    if (materia.trim() === "") return;
+
+    const nuevaMateria = {
+      nombre: materia,
+      promedio: 0,
+    };
+
+    setMaterias([...materias, nuevaMateria]);
+
+    setMateria("");
+  };
+
   return (
     <div className="window">
+
       <h1>Materias</h1>
+
+      {/* FORMULARIO */}
+      <div className="form-add">
+
+        <input
+          type="text"
+          placeholder="Nombre de la materia"
+          value={materia}
+          onChange={(e) => setMateria(e.target.value)}
+        />
+
+        <button onClick={agregarMateria}>
+          Agregar
+        </button>
+
+      </div>
+
+      {/* LISTA */}
+      <div className="subjects-list">
+
+        {materias.map((item, index) => (
+
+          <div className="subject-card" key={index}>
+
+            <span>{item.nombre}</span>
+
+            <span>
+              Promedio: {item.promedio}
+            </span>
+
+          </div>
+
+        ))}
+
+      </div>
+
     </div>
   );
 }
